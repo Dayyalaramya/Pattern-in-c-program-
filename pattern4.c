c1fb8d3508ab06314f19282c80c348c4f21702bc
Dayyalaramya <ramyadayyala8@gmail.com>
2026-02-15 10:37:53 +0530

    Add files via upload

---
+++ pattern4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int row,col;
+printf("enter the number of rows and columns:\n");
+scanf("%d %d",&row,&col);
+for(int i=0;i<=row-1;i++)
+{
+ for(int j=0;j<=col-1;j++)
+{
+  if(i==row-1 || j==col-1 || i+j==row-1)
+   {
+   printf("* ");
+   }
+   else 
+   {
+   printf("  "); 
+   }
+   }
+   printf("\n");
+    }
+   
+return 0;
+}
+
+
+
+
+
+
+
+    
